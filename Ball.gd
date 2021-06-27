d8b9ff0df14c564429adfde58926a431bf548c30
John Pendergrass <johnpendergrass@gmail.com>
2021-06-27 15:50:45 -0700

    Refactor screensize getting

--- Ball.gd
+++ Ball.gd
@@ -4,10 +4,7 @@ extends Node2D
 export var speed : int = 200
 
 
-
 var velocity: Vector2 = Vector2(1,1)
-var screen_width : int = 1920
-var screen_height : int = 960
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -18,18 +15,9 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):	
-	# var velocity = Vector2()
-	
-	# velocity.x = 1
-	# velocity.y = 1
-	speed += 1
-	if speed > 25000:
-		speed = 25000
-	
-	
-	if position.x > screen_width or position.x < 0:
+	if position.x > OS.window_size.x or position.x < 0:
 		velocity.x *= -1
-	if position.y > screen_height or position.y < 0:
+	if position.y > OS.window_size.y or position.y < 0:
 		velocity.y *= -1
 
 	velocity = velocity.normalized() * speed
